ed7baff18581df9deccaa11da86df036ab53ec8f
Tony Bowden <tony@tmtm.com>
2019-01-30 16:28:48 +0000

    Add 2013 frwiki data

--- scraper.rb
+++ scraper.rb
@@ -3,16 +3,21 @@
 
 require 'wikidata/fetcher'
 
-names_2013 = EveryPolitician::Wikidata.wikipedia_xpath(
+lb2013 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://lb.wikipedia.org/wiki/Chamber',
   after: '//h3[contains(.,"Aktuell Period")]',
   before: '//h3[contains(.,"Period 2009-2013")]',
   xpath: '//li//a[not(@class="new")]/@title',
 )
 
-names_2016 = EveryPolitician::Wikidata.wikipedia_xpath(
+fr2013 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://fr.wikipedia.org/wiki/Modèle:Palette_Députés_de_la_Chambre_des_députés_du_Luxembourg_2018-2023',
-  xpath: '//table//td[@class="navbox-list"]//a[not(@class="new")]/@title'
+  xpath: '//table//td[contains(@class,"navbox-list")]//a[not(@class="new")]/@title'
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { lb: names_2013, fr: names_2016 })
+fr2016 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://fr.wikipedia.org/wiki/Modèle:Palette_Députés_de_la_Chambre_des_députés_du_Luxembourg_2013-2018',
+  xpath: '//table//td[contains(@class,"navbox-list")]//a[not(@class="new")]/@title'
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { lb: lb2013, fr: fr2013 | fr2016 })
